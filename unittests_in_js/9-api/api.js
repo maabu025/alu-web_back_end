a5e4e476ed4415c3d7147dbca9a11f704573bfe9
maabu025 <m.abu@alustudent.com>
2025-11-08 12:10:06 +0000

    unittests_in_js

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.end('Welcome to the payment system');
 });
 
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`);
+});
+
 app.listen(port, () => {
   console.log('API available on localhost port 7865');
 });
