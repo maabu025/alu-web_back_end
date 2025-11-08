a0db2750cf8061745d34b7f798d3f0fdd06b8816
maabu025 <m.abu@alustudent.com>
2025-11-08 14:11:31 +0000

    ES6_promise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,6 +1,14 @@
-export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
+export default function (boolean) {
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (boolean === true) {
+      resolve(object);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
